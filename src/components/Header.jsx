7a88d06f621b4979e0a02978029100b32ef9687b
GrupoUnoNeoland <grupounoneoland@gmail.com>
2024-02-28 19:22:22 -0300

    Header compoent

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import "./Header.css";
+import { NavLink, Link } from "react-router-dom";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div id="header-container">
+      <Link to="/">
+        <img
+          className="header-logo"
+          src="https://res.cloudinary.com/dqiveomlb/image/upload/v1709122389/APP/logo_v7d6ui.png"
+          alt="logo"
+        />
+      </Link>
+      <nav className="header-login">
+        <NavLink to="/register">
+          <div>REGISTER</div>
+        </NavLink>
+        <NavLink to="/login">
+          <div>LOGIN</div>
+        </NavLink>
+      </nav>
+    </div>
+  );
 };
