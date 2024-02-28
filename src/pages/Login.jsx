db5a679e13249b6731578adc04ba8019d508331c
GrupoUnoNeoland <grupounoneoland@gmail.com>
2024-02-28 19:22:52 -0300

    Login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import "./Login.css";
+import { Link, Navigate } from "react-router-dom";
+import { useAuth } from "../context/authContext";
+import { loginUser } from "../services/user.service";
+import { useLoginError } from "../hooks";
+
+export const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const [send, setSend] = useState(false);
+  const [res, setRes] = useState({});
+  const [loginOk, setLoginOk] = useState(false);
+  const { login, setUser } = useAuth();
+
+  const formSubmit = async (formData) => {
+    console.log("FORMDATA", formData);
+    setSend(true);
+    setRes(await loginUser(formData));
+    setSend(false);
+  };
+
+  useEffect(() => {
+    console.log(res);
+    useLoginError(res, setRes, login, setLoginOk);
+  }, [res]);
+
+  useEffect(() => {
+    setUser(() => null);
+    localStorage.removeItem("user");
+  }, []);
+
+  if (loginOk) {
+    if (res.data?.user?.check == false) {
+      return <Navigate to="/verifyCode" />;
+    } else {
+      return <Navigate to="/dashboard" />;
+    }
+  }
+  return (
+    <div id="login-container">
+      <div className="form-wrap">
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit(formSubmit)}>
+          <div className="email_container form-group">
+            <input
+              className="input_user"
+              type="email"
+              id="email"
+              name="email"
+              autoComplete="false"
+              {...register("email", { required: true })}
+            />
+            <label htmlFor="custom-input" className="custom-placeholder">
+              email
+            </label>
+          </div>
+          <div className="password_container form-group">
+            <input
+              className="input_user"
+              type="password"
+              id="password"
+              name="password"
+              autoComplete="false"
+              {...register("password", { required: true })}
+            />
+            <label htmlFor="custom-input" className="custom-placeholder">
+              password
+            </label>
+          </div>
+          <div className="btn_container">
+            <button
+              className="btn"
+              type="submit"
+              disabled={send}
+              style={{ background: send ? "#666" : "#333" }}
+            >
+              LOGIN
+            </button>
+          </div>
+          <div>
+            <p className="bottom-text">
+              <small>Have you forgotten the password?</small>
+            </p>
+            <p className="bottom-text">
+              <small>
+                <Link to="/forgotpassword" className="anchorCustom">
+                  Change password
+                </Link>
+              </small>
+            </p>
+          </div>
+
+          <div className="footerForm">
+            <p className="parrafoLogin">Are you not registered?</p>
+            <p className="parrafoLogin">
+              <Link to="/register">Register Here</Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
