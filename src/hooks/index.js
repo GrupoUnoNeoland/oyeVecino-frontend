7e06c13acf2ae55632cc9547978c7b116109c4c0
Natalia <naticastecno@gmail.com>
2024-03-02 21:06:33 +0100

    create hook resend code error

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,6 @@
+export * from './useAutoLogin';
 export * from './useLoginError';
 export * from './useRegisterError';
+export * from './useCheckCodeError';
+export * from './useResendCodeError';
+export * from './useForgotPasswordError';
