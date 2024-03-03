a2104e130d415b8dd2c1115b335328bab1d7f46f
GrupoUnoNeoland <grupounoneoland@gmail.com>
2024-03-03 17:13:51 -0300

    update protected login routes

--- src/components/ProtectedRoute/Protected.jsx
+++ src/components/ProtectedRoute/Protected.jsx
@@ -1,14 +1,20 @@
+import React from 'react';
 import { Navigate } from 'react-router-dom';
-
 import { useAuth } from '../../context/authContext';
+
+
 
 export const Protected = ({ children }) => {
   const { user, deleteUser } = useAuth();
+  console.log(user)
   if (deleteUser) {
     return <Navigate to="/register" />;
   }
   if (user == null || user?.check == false) {
     return <Navigate to="/login" />;
+  }
+  if (user?.request.length == 0) {
+    return <Navigate to="/request" />;
   }
 
   return children;
