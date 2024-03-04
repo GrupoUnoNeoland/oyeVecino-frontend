0be408469b23377ae89096d1f38f9fc47402a755
GrupoUnoNeoland <grupounoneoland@gmail.com>
2024-03-04 06:05:14 -0300

    update protected check code and request route

--- src/components/ProtectedRoute/ProtectedCheckChildren.jsx
+++ src/components/ProtectedRoute/ProtectedCheckChildren.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router-dom';
-
+import React from 'react';
 import { useAuth } from '../../context/authContext';
 
 export const ProtectedCheckChildren = ({ children }) => {
