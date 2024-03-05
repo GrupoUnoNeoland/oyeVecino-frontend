a61cdf188a3e1cef37f8a9fd088695648d350bb0
GrupoUnoNeoland <grupounoneoland@gmail.com>
2024-03-05 19:02:11 -0300

    add hamburguer menu to mobile version

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,26 +3,38 @@ import './Header.css';
 import { Link, NavLink } from 'react-router-dom';
 
 import { useAuth } from '../context/authContext';
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Header = () => {
   const { user } = useAuth();
+  const [showMenuMob, setShowMenuMob] = useState(false)
+  console.log(showMenuMob)
+
+  const handleClickMenu = () => {
+    setShowMenuMob(!showMenuMob)
+  }
   return (
+    <>
     <div id="header-container">
-      <Link to="/">
+      <div className="nav-fixed">
+      <Link to={user ? "/dashboard" : "/"}>
         <img
           className="header-logo"
           src="https://res.cloudinary.com/dqiveomlb/image/upload/v1709122389/APP/logo_v7d6ui.png"
           alt="logo"
         />
       </Link>
+      <i className="fa fa-bars" onClick={handleClickMenu}></i>
+      {/* <div className="hamburguer-menu-container">
+        <i class="fa fa-bars"></i>
+      </div> */}
       {!user && (
         <nav className="header-login">
           <NavLink to="/register">
-            <div>REGISTER</div>
+            <div className="header-nav__item">REGISTER</div>
           </NavLink>
           <NavLink to="/login">
-            <div>LOGIN</div>
+            <div className="header-nav__item">LOGIN</div>
           </NavLink>
         </nav>
       )}
@@ -39,6 +51,31 @@ export const Header = () => {
           </NavLink>
         </nav>
       )}
+      </div>
+      <nav className={showMenuMob ? "nav-mobile" : "nav-mobile hidden"}>
+      {!user && (
+        <>
+          <NavLink to="/register">
+            <div className="header-nav__item--mobile" onClick={handleClickMenu}>REGISTER</div>
+          </NavLink>
+          <NavLink to="/login">
+            <div className="header-nav__item--mobile" onClick={handleClickMenu}>LOGIN</div>
+          </NavLink>
+          </>
+      )}
+          {user && (
+            <>
+            <NavLink to="/profile">
+              <div className="header-nav__item--mobile" onClick={handleClickMenu}>PROFILE</div>
+            </NavLink>
+          <NavLink to="/login">
+            <div className="header-nav__item--mobile" onClick={handleClickMenu}>LOGOUT</div>
+          </NavLink>
+        </>
+          )}
+      </nav>
     </div>
+    
+    </>
   );
 };
