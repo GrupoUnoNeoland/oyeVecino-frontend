5fa8968bb44b72e2c63983548aceca61ea8c74ae
GrupoUnoNeoland <grupounoneoland@gmail.com>
2024-03-06 17:41:39 -0300

    update dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,69 +1,99 @@
 import './Dashboard.css';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link} from 'react-router-dom';
+import { getAllServices } from '../services/service.service';
+import { getAllStatements } from '../services/Statement.service';
+import { getAllEvents } from '../services/Events.service';
 
 export const Dashboard = () => {
-  return (
-    <>
-      <div id="dashboard-container">
-        <h1>
-          Bienvenidxs a <span>OYEVECINO!</span>
-        </h1>
-        <div>
-          <img
-            className="backpicture"
-            src="https://res.cloudinary.com/dqiveomlb/image/upload/v1709593054/APP/tony-lee-8IKf54pc3qk-unsplash_zwcr7i.jpg"
-            alt="neigbhorhood"
-          />
-        </div>
+const [content, setContent] = useState(null)
+console.log(content)
 
-        <div className="web-sections">
-          <Link to="/serviceOffered">
-            {' '}
-            {/*mirar ruta*/}
-            <div className="box-service-offered">
-              <h2>SERVICIOS OFRECIDOS</h2>
-              <p>
-                Bienvenido a nuestro querido vecindario, donde la comunidad se une para
-                ofrecerte servicios excepcionales que harán tu vida más cómoda y
-                agradable. En nuestra vibrante sección de servicios, encontrarás una
-                variedad de opciones pensadas especialmente para ti.
-              </p>
+const getAllServ = async (type) => {
+  setContent(null)
+  const resServ = await getAllServices(type)
+  const services = resServ.data
+  setContent({item: "service", data: services})
+}
+
+const getAllStat = async () => {
+  setContent(null)
+  const resStat = await getAllStatements()
+  const statements = resStat.data
+  setContent({item: "statment", data: statements})
+}
+
+const getAllEven = async () => {
+  setContent(null)
+  const resEven = await getAllEvents()
+  const events = resEven.data
+  setContent({item: "event", data: events})
+}
+
+const handleClickBtnDashboard = async (option) => {
+  switch (option) {
+    case "ofredServ":
+        await getAllServ("offered")
+      break;
+    case "demanServ":
+        await getAllServ("demanded")
+      break;
+    case "statem":
+        await getAllStat()
+      break;
+      case "event":
+        await getAllEven()
+       break;
+  }
+}
+
+  return (
+      <div id="dashboard-container">
+        <section className="dashboard__buttons">
+          <button className="servicios-ofrecidos--btn dashboard__button" onClick={() => handleClickBtnDashboard("ofredServ")}>Servicios Ofrecidos</button>
+          <button className="servicios-demandados--btn dashboard__button" onClick={() => handleClickBtnDashboard("demanServ")}>Servicios Demandados</button>
+          <button className="comunicados--btn dashboard__button" onClick={() => handleClickBtnDashboard("statem")}>Comunicados</button>
+          <button className="eventos--btn dashboard__button" onClick={() => handleClickBtnDashboard("event")}>Eventos</button>
+        </section>
+        <section className="dashboard__content">
+          {content != null && content.item == "service" && content.data.map(item => (
+          <div key={item._id} className="dashboard__card">
+            <div className="card__header">
+              <img src={item.images[0]} alt="image" />
             </div>
-          </Link>
-          <Link to="/serviceDemanded">
-            <div className="box-service-demanded">
-              <h2>SERVICIOS DEMANDADOS</h2>
-              <p>
-                Vecinos, ¿alguna vez has deseado servicios que se adapten perfectamente a
-                tus necesidades y estén a solo unos pasos de distancia? Pide lo que
-                necesitas.
-              </p>
+            <div className="card__body">
+              <h4>{item.title}</h4>
+              <p>{item.description}</p>
+              <Link to={`/service/${item._id}`}><p className="card__body-detail">Ver más...</p></Link>
             </div>
-          </Link>
-          <Link to="/events">
-            <div className="box-events">
-              <h2>EVENTOS</h2>
-              <p>
-                ¿Te has preguntado qué está sucediendo en nuestra comunidad? ¡Aquí te
-                contamos! Desde divertidas ferias de vecindario hasta eventos culturales y
-                actividades familiares, nuestro barrio es el epicentro de la diversión.
-              </p>
+          </div>
+          ))}
+          {content != null && content.item == "statment" && content.data.map(item => (
+          <div key={item._id} className="dashboard__card">
+            <div className="card__header">
+              <img src={item.images[0]} alt="image" />
             </div>
-          </Link>
-          <Link to="/statements">
-            <div className="box-statements">
-              <h2>COMUNICADOS</h2>
-              <p>
-                Bienvenidos a nuestra nueva sección de Comunicados, el lugar donde la voz
-                de nuestro barrio cobra vida. Aquí, cada uno de ustedes tiene la
-                oportunidad de compartir noticias, anuncios emocionantes y hasta esos
-                pequeños detalles que hacen que nuestra comunidad sea tan especial.
-              </p>
+            <div className="card__body">
+              <h4>{item.title}</h4>
+              <p>{item.description}</p>
+              <Link to={`/service/${item._id}`}><p className="card__body-detail">Ver más...</p></Link>
             </div>
-          </Link>
-        </div>
+          </div>
+          ))}
+          {content != null && content.item == "event" && content.data.map(item => (
+          <div key={item._id} className="dashboard__card">
+            <div className="card__header">
+              <img src={item.images[0]} alt="image" />
+            </div>
+            <div className="card__body">
+              <h4>{item.title}</h4>
+              <p>{item.description}</p>
+              <Link to={`/service/${item._id}`}><p className="card__body-detail">Ver más...</p></Link>
+            </div>
+          </div>
+          ))}
+          
+        </section>
       </div>
-    </>
   );
 };
