dffbce0d468dfc9c9d289518a89361e3db92214a
GrupoUnoNeoland <grupounoneoland@gmail.com>
2024-03-06 17:41:13 -0300

    create component TimeStamps

---
+++ src/components/TimeStamps.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './TimeStamps.css';
+export const TimeStamps = ({ createdAt }) => {
+  const creationDate = new Date(createdAt);
+
+  const formatCreationDate = creationDate.toLocaleString('es-ES', {
+    month: '2-digit',
+    day: '2-digit',
+    year: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+  return (
+    <div>
+      <p>{formatCreationDate}</p>
+    </div>
+  );
+};
