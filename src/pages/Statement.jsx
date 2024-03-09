069a131ead71dafb4b232d76ddd7310924f5c101
Natalia <naticastecno@gmail.com>
2024-03-09 18:26:39 +0100

    create statement detail page

--- src/pages/Statement.jsx
+++ src/pages/Statement.jsx
@@ -1,46 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import './Statement.css';
-import { getByIdService } from '../services/service.service';
+import { getByIdStatements } from '../services/Statement.service';
 import { useParams } from 'react-router-dom';
-import { Rating } from 'primereact/rating';
 import { useAuth } from '../context/authContext';
-import { createRating } from '../services/Rating.service';
+import { createLike, deleteLike } from '../services/Like.service';
 import { TimeStamps } from '../components/TimeStamps';
 import { useForm } from 'react-hook-form';
 import { createMessage } from '../services/message.service';
+import { toggleLikeInStatement } from '../services/Statement.service';
 
 export const Statement = () => {
-  const [service, setService] = useState(null);
-  const [rating, setRating] = useState(null);
+  const [statement, setStatement] = useState(null);
+  const [like, setLike] = useState(false);
   const [message, setMessage] = useState(null);
+
   const { register, handleSubmit, reset } = useForm();
   const { user } = useAuth();
-
   const { id } = useParams();
-  const formDataRating = JSON.stringify({ serviceId: id, stars: rating });
+  console.log(user);
+
+  const checkUserLike = () => {
+    if (statement != null) {
+      console.log(statement.likes);
+      const likeUser = statement?.likes.find((like) => like._id == user._id);
+      likeUser ? setLike(true) : setLike(false);
+    }
+  };
 
   const formSubmit = async (formData) => {
-    formData.type = 'service';
-    console.log('FORMDATA', formData);
+    console.log(formData);
+    formData.type = 'statement';
     setMessage(await createMessage(formData, id));
   };
 
-  const getService = async (id) => {
-    setService(null);
-    const resService = await getByIdService(id);
-    const service = resService.data;
-    setService(service);
+  const getStatement = async (id) => {
+    console.log('getStatemnt');
+    setStatement(null);
+    const resStatement = await getByIdStatements(id);
+    const statementData = resStatement.data;
+    setStatement(statementData);
+  };
+
+  const handleLikeClick = async () => {
+    const formDataLike = JSON.stringify({ statementsFav: user._id });
+    await toggleLikeInStatement(id, formDataLike);
+    setLike(!like);
   };
 
   useEffect(() => {
-    getService(id);
+    getStatement(id);
   }, [message]);
 
   useEffect(() => {
-    if (rating != null) {
-      createRating(formDataRating);
-    }
-  }, [rating]);
+    checkUserLike();
+  }, [statement]);
 
   useEffect(() => {
     if (message != null) {
@@ -49,28 +62,28 @@ export const Statement = () => {
   }, [message]);
 
   return (
-    <div id="servicepage-container">
-      {service ? (
-        <div className="service_container" key={service._id}>
-          <div className="service_user">
-            <div className="provider-photo_container">
-              <img src={service.provider[0].image} alt="" />
+    <div id="statementpage-container">
+      {statement ? (
+        <div className="statement_container" key={statement._id}>
+          <div className="statement_user">
+            <div className="owner-photo_container">
+              <img src={statement.owner[0]?.image} alt="" />
             </div>
-            <p>{service.provider[0].name}</p>
+            <p>{statement.owner[0]?.name}</p>
           </div>
-          <div className="service_container-body">
-            <h2 className="service_title">{service.title}</h2>
-            <div className="service_description">
-              <p>{service.description}</p>
+          <div className="statement_container-body">
+            <h2 className="statement_title">{statement.title}</h2>
+            <div className="statement_description">
+              <p>{statement.description}</p>
             </div>
-            <p className="service_photos-title">FOTOS</p>
-            {service.images.map((image, index) => (
-              <div key={index} className="service-photo_container">
+            <p className="statement_photos-title">FOTOS</p>
+            {statement.images.map((image, index) => (
+              <div key={index} className="statement-photo_container">
                 <img src={image} alt="image" />
               </div>
             ))}
             <div className="comments_container">
-              <h2>Comentário Público</h2>
+              <h2>Comentario Público</h2>
               <form onSubmit={handleSubmit(formSubmit)}>
                 <div className="comment_input">
                   <div className="comment_avatar">
@@ -84,17 +97,19 @@ export const Statement = () => {
                     {...register('content', { required: true })}
                   />
                 </div>
-                <div className="comment_stars">
-                  <Rating
-                    value={rating}
-                    disabled={rating != null && true}
-                    onChange={(e) => setRating(e.value)}
-                    cancel={false}
-                  />
-                </div>
                 <button type="submit">Comentar</button>
               </form>
-              {service?.comments.map((item, index) => (
+              <div className="like_button_container">
+                <button
+                  className={like ? 'like-button liked' : 'like-button'}
+                  onClick={handleLikeClick}
+                >
+                  <span role="img" aria-label="heart" className="heart-icon">
+                    &#x2665;
+                  </span>
+                </button>
+              </div>
+              {statement?.comments.map((item, index) => (
                 <div key={index} className="comment_allcoment">
                   <div className="comment_user-photo">
                     <img src={item.owner.image} alt="image-avatar" />
