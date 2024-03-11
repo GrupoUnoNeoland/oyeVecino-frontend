dd61238a4546f2a91e9f0ad06ede079e7c35c9e1
Clara Palmero <clarapalmero@email.com>
2024-03-11 18:47:34 +0100

    event likes update

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -11,7 +11,6 @@ export const Event = () => {
   const [event, setEvent] = useState(null);
   const [like, setLike] = useState(false);
   const [message, setMessage] = useState(null);
-
   const { register, handleSubmit, reset } = useForm();
   const { user } = useAuth();
   const { id } = useParams();
@@ -31,19 +30,38 @@ export const Event = () => {
     setMessage(await createMessage(formData, id));
   };
 
-  const getEvent = async (id) => {
+ const getEvent = async (id) => {
     console.log('getEvent');
-    setEvent(null);
+
     const resEvent = await getByIdEvents(id);
-    const eventData = resEvent.data;
-    setEvent(eventData);
+
+    if (resEvent.status == 200) {
+      const eventData = resEvent.data;
+      console.log(resEvent);
+      setEvent(eventData);
+    } else {
+      getEvent(id);
+    }
   };
 
   const handleLikeClick = async () => {
     const formDataLike = JSON.stringify({ eventsFav: user._id });
     await toggleLikeInEvent(id, formDataLike);
+
+     if (event.likes?.find((item) => item._id == user._id)) {
+      setEvent((value) => {
+        const updateLike = value?.likes?.filter((item) => item._id != user._id);
+        console.log(updateLike);
+        return { ...value, likes: updateLike };
+      });
+      getEvent(id);
+    } else {
+      setEvent((value) => ({ ...value, likes: [...value.likes, user] }));
+      getEvent(id);
+    }
     setLike(!like);
   };
+  
 
   useEffect(() => {
     getEvent(id);
@@ -65,21 +83,32 @@ export const Event = () => {
         <div className="event_container" key={event._id}>
           <div className="event_user">
             <div className="organizer-photo_container">
-              <img src={event.organizer[0]?.image} alt="" />
+              <img src={event?.organizer[0]?.image} alt="" />
             </div>
-            <p>{event.organizer[0]?.name}</p>
+            <p>{event?.organizer[0]?.name}</p>
           </div>
           <div className="event_container-body">
-            <h2 className="event_title">{event.title}</h2>
+            <h2 className="event_title">{event?.title}</h2>
             <div className="event_description">
-              <p>{event.description}</p>
+              <p>{event?.description}</p>
             </div>
             <p className="event_photos-title">FOTOS</p>
-            {event.images.map((image, index) => (
+            {event?.images?.map((image, index) => (
               <div key={index} className="event-photo_container">
                 <img src={image} alt="image" />
               </div>
             ))}
+            <div className="like_button_container">
+              <button
+                className={like ? 'like-button liked' : 'like-button'}
+                onClick={(e) => handleLikeClick(e)}
+              >
+                <span role="img" aria-label="heart" className="heart-icon">
+                  &#x2665;
+                </span>
+              </button>
+              <p>{event?.likes?.length}</p>
+            </div>
             <div className="comments_container">
               <h2>Comentario Público</h2>
               <form onSubmit={handleSubmit(formSubmit)}>
@@ -97,20 +126,11 @@ export const Event = () => {
                 </div>
                 <button type="submit">Comentar</button>
               </form>
-              <div className="like_button_container">
-                <button
-                  className={like ? 'like-button liked' : 'like-button'}
-                  onClick={handleLikeClick}
-                >
-                  <span role="img" aria-label="heart" className="heart-icon">
-                    &#x2665;
-                  </span>
-                </button>
-              </div>
+              
               {event?.comments.map((item, index) => (
                 <div key={index} className="comment_allcoment">
                   <div className="comment_user-photo">
-                    <img src={item.owner.image} alt="image-avatar" />
+                    <img src={item.organizer.image} alt="image-avatar" />
                   </div>
                   <div className="comment_infos">
                     <div className="comment_text">{item.content}</div>
@@ -123,5 +143,5 @@ export const Event = () => {
         </div>
       ) : null}
     </div>
-  );
+    );
 };
