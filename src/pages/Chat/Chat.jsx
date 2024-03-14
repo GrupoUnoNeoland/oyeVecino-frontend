5f257a96eb073966ff3345f256b992efd221014a
JuFML <julianafernandezlima@hotmail.com>
2024-03-14 18:59:13 -0300

    update chat page

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,11 +1,10 @@
-import { useLocation, useParams } from 'react-router-dom';
+import './Chat.css';
+import { useLocation } from 'react-router-dom';
 import { useAuth } from '../../context/authContext';
 import { getById } from '../../services/user.service';
-import './Chat.css';
 import React, { useEffect, useState } from 'react';
 import { createMessage } from '../../services/message.service';
 import { useForm } from 'react-hook-form';
-import { Register } from '../Register';
 import { TimeStamps } from '../../components/TimeStamps';
 
 export const Chat = () => {
@@ -14,24 +13,24 @@ export const Chat = () => {
   const [activeChat, setActiveChat] = useState();
   const [resMessage, setResMessage] = useState();
   const [chatId, setChatId] = useState();
+  const [isMobile, setIsMobile] = useState(false);
   const [recipientMessageId, setRecipientMessageId] = useState();
   const [messageSend, setMessageSend] = useState(false);
   console.log('activeChat:', activeChat);
   const { register, handleSubmit, reset } = useForm();
+  console.log(isMobile);
 
   const { user } = useAuth();
-  //console.log('🚀userLogged:', userLogged);
-  //console.log('activeChat:', activeChat);
-  //console.log('resMe', resMessage);
-  // console.log('2', chatId);
-  //console.log('6', userLogged);
-  //console.log('8', recipientMessageId);
-  //console.log('9', activeChat);
 
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const idProvider = queryParams.get('id');
   const chatIdParams = queryParams.get('chatId');
+
+  const checkWindowWidth = () => {
+    let windowWidth = window.innerWidth;
+    setIsMobile(() => (windowWidth <= 700 ? true : false));
+  };
 
   const getByIdProvider = async (id) => {
     setProvider(null);
@@ -39,18 +38,15 @@ export const Chat = () => {
 
     if (resProvider?.status == 200) {
       const provider = resProvider.data;
-
       setProvider(provider);
     }
   };
 
   const getByIdUserLogged = async (id) => {
     const resUserLogged = await getById(id);
-    //console.log('4');
-    if (resUserLogged?.status == 200) {
-      //console.log('5');
-      const userLogged = resUserLogged.data;
 
+    if (resUserLogged?.status == 200) {
+      const userLogged = resUserLogged.data;
       setUserLogged(userLogged);
     }
   };
@@ -60,11 +56,14 @@ export const Chat = () => {
     setResMessage(await createMessage(formData, recipientMessageId));
     setMessageSend(true);
   };
-  const handleClickChatItem = (chatId, recipientId) => {
-    //console.log('7', chatId);
 
+  const handleClickChatItem = (chatId, recipientId) => {
+    // if (!isMobile) {
     setRecipientMessageId(recipientId);
     setActiveChat(userLogged?.chats?.find((chat) => chat?._id == chatId));
+    // } else {
+    //   console.log('MB');
+    // }
   };
 
   useEffect(() => {
@@ -74,22 +73,21 @@ export const Chat = () => {
     } else {
       getByIdUserLogged(user._id);
     }
-    //getByIdUserLogged(user._id);
   }, []);
 
   useEffect(() => {
     if (chatIdParams) {
-      //console.log('1', chatIdParams);
       setChatId(chatIdParams);
     }
+    checkWindowWidth();
   }, []);
+
   useEffect(() => {
     if (chatId) {
-      //console.log('3', chatId);
       getByIdUserLogged(user._id);
-      //handleClickChatItem(chatId, idProvider);
     }
   }, [chatId]);
+
   useEffect(() => {
     if (messageSend) {
       getByIdUserLogged(user._id);
@@ -97,9 +95,9 @@ export const Chat = () => {
       setMessageSend(false);
     }
   }, [messageSend]);
+
   useEffect(() => {
     if (userLogged) {
-      //console.log('useEfUserLogged');
       chatId
         ? handleClickChatItem(chatId, idProvider)
         : handleClickChatItem(activeChat?._id, recipientMessageId);
@@ -116,48 +114,54 @@ export const Chat = () => {
   return (
     <div id="chat_container">
       <aside className="actives-chats-container">
-        <header>
+        <div className="chats-container-header">
           <h2>Tus Chats</h2>
-        </header>
-        {userLogged?.chats?.map((chat) =>
-          chat?.userOne[0]?._id == user?._id ? (
-            <div
-              className="chat-item"
-              key={chat._id}
-              onClick={() => handleClickChatItem(chat._id, chat?.userTwo[0]._id)}
-            >
-              <div className="image-provider-container">
-                <img
-                  src={chat?.userTwo[0]?.image}
-                  alt={chat?.userTwo[0]?.name}
-                  className="image-provider"
-                />
+        </div>
+        <div className="chats">
+          {userLogged?.chats?.map((chat) =>
+            chat?.userOne[0]?._id == user?._id ? (
+              <div
+                className="chat-item"
+                key={chat._id}
+                onClick={() => handleClickChatItem(chat._id, chat?.userOne[0]._id)}
+              >
+                <div className="image-provider-container">
+                  <img
+                    src={chat?.userTwo[0]?.image}
+                    alt={chat?.userTwo[0]?.name}
+                    className="image-provider"
+                  />
+                </div>
+                <h2>{chat?.userTwo[0]?.name}</h2>
               </div>
-              <h2>{chat?.userTwo[0]?.name}</h2>
-            </div>
-          ) : (
-            <div
-              className="chat-item"
-              key={chat._id}
-              onClick={() => handleClickChatItem(chat._id, chat?.userOne[0]._id)}
-            >
-              <div className="image-provider-container">
-                <img
-                  src={chat?.userOne[0]?.image}
-                  alt={chat?.userOne[0]?.name}
-                  className="image-provider"
-                />
+            ) : (
+              <div
+                className="chat-item"
+                key={chat._id}
+                onClick={() => handleClickChatItem(chat._id, chat?.userOne[0]._id)}
+              >
+                <div className="image-provider-container">
+                  <img
+                    src={chat?.userOne[0]?.image}
+                    alt={chat?.userOne[0]?.name}
+                    className="image-provider"
+                  />
+                </div>
+                <h3>{chat?.userOne[0]?.name}</h3>
               </div>
-              <h2>{chat?.userOne[0]?.name}</h2>
-            </div>
-          ),
-        )}
+            ),
+          )}
+        </div>
       </aside>
-      <div className="main-content">
-        {activeChat ? (
-          activeChat?.userOne[0]?._id == user?._id ? (
+
+      {activeChat ? (
+        activeChat?.userOne[0]?._id == user?._id ? (
+          <div className="main-content">
             <section className="messages-container">
-              <header>
+              <div className="messages-header-container">
+                <button className="messages-header-btn" onClick={() => setActiveChat()}>
+                  <span className="material-symbols-outlined">arrow_back</span>
+                </button>
                 <div className="image-provider-container">
                   <img
                     src={activeChat?.userTwo[0]?.image}
@@ -166,17 +170,42 @@ export const Chat = () => {
                   />
                 </div>
                 <h2>{activeChat?.userTwo[0]?.name}</h2>
-              </header>
-              {activeChat?.messages?.map((message) => (
-                <div className="message-container" key={message._id}>
-                  <p>{message?.content}</p>
-                  <TimeStamps createdAt={message?.createdAt} />
-                </div>
-              ))}
+              </div>
+              <div className="messages">
+                {activeChat?.messages?.map((message) => (
+                  <div
+                    className={
+                      message?.owner == user._id
+                        ? 'message-container message-container--right'
+                        : 'message-container message-container--left'
+                    }
+                    key={message._id}
+                  >
+                    <p className="message-content">{message?.content}</p>
+                    <TimeStamps createdAt={message?.createdAt} />
+                  </div>
+                ))}
+              </div>
+              <div className="messages-footer-container">
+                <form onSubmit={handleSubmit(formSubmit)}>
+                  <div className="input-container">
+                    <input
+                      type="text"
+                      name="message"
+                      id="message"
+                      onClick={() => reset()}
+                      {...register('content', { required: true })}
+                    />
+                    <button>Enviar</button>
+                  </div>
+                </form>
+              </div>
             </section>
-          ) : (
+          </div>
+        ) : (
+          <div className="main-content">
             <section className="messages-container">
-              <header>
+              <div className="messages-header-container">
                 <div className="image-provider-container">
                   <img
                     src={activeChat?.userOne[0]?.image}
@@ -185,32 +214,40 @@ export const Chat = () => {
                   />
                 </div>
                 <h2>{activeChat?.userOne[0]?.name}</h2>
-              </header>
-              {activeChat?.messages?.map((message) => (
-                <div className="message-container" key={message._id}>
-                  <p>{message?.content}</p>
-                  <TimeStamps createdAt={message?.createdAt} />
-                </div>
-              ))}
+              </div>
+              <div className="messages">
+                {activeChat?.messages?.map((message) => (
+                  <div
+                    className={
+                      message?.owner == user._id
+                        ? 'message-container message-container--right'
+                        : 'message-container message-container--left'
+                    }
+                    key={message._id}
+                  >
+                    <p className="message-content">{message?.content}</p>
+                    <TimeStamps createdAt={message?.createdAt} />
+                  </div>
+                ))}
+              </div>
+              <div className="messages-footer-container">
+                <form onSubmit={handleSubmit(formSubmit)}>
+                  <div className="input-container">
+                    <input
+                      type="text"
+                      name="message"
+                      id="message"
+                      onClick={() => reset()}
+                      {...register('content', { required: true })}
+                    />
+                    <button>Enviar</button>
+                  </div>
+                </form>
+              </div>
             </section>
-          )
-        ) : null}
-
-        <footer>
-          <form onSubmit={handleSubmit(formSubmit)}>
-            <div className="input-container">
-              <input
-                type="text"
-                name="message"
-                id="message"
-                onClick={() => reset()}
-                {...register('content', { required: true })}
-              />
-              <button>Enviar</button>
-            </div>
-          </form>
-        </footer>
-      </div>
+          </div>
+        )
+      ) : null}
     </div>
   );
 };
