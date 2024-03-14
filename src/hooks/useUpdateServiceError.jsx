3e4597402410f6f5d67cdfbd00f21d3aa75782f0
NachoGracia <gracianacho@gmail.com>
2024-03-14 01:23:21 +0100

    update service error page

---
+++ src/hooks/useUpdateServiceError.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Swal from 'sweetalert2/dist/sweetalert2.all.js';
+
+export const useUpdateServiceError = (res, setRegisterOk, setRes) => {
+  //? si la respuesta es ok ---- > directamente esta el status en la primera clave es decir: res.status
+  //? si la respuesta no esta ok--> res.response.status
+  //! ------------------ 200 : todo ok
+  if (res?.status == 200) {
+    const dataToString = JSON.stringify(res);
+    localStorage.setItem('data', dataToString);
+    setRegisterOk(() => true);
+
+    Swal.fire({
+      icon: 'success',
+      title: 'Servicio actualizado con éxito ! 👏',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    setRes({});
+  }
+  //! -------------------- 500 : internal server error
+
+  if (res?.response?.status == 500) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Interval server error!❎ Please try again.',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    setRes({});
+  }
+
+  //! -------------------- 404: 'error, event not created'
+  if (res?.response?.status == 404) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Servicio no actualizado ! ❌',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    setRes({});
+  }
+};
